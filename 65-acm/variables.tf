781b98e556535957ec668bccf401609e12229f3e
Venkatesh <tvenky359@gmail.com>
2025-12-18 07:04:05 +0530

    Changes added

---
+++ 65-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_id" {
+  default = "Z0289756D5X1LXL2LSE7"
+}
+
+variable "domain_name" {
+  default = "venkatesh.fun"
+}
